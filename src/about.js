497148dfe2145ff1fa11738d15730c933030c42f
Viktordotcom <adric.jawad@gmail.com>
2021-08-19 10:42:47 +0200

    Fixed a typo

--- src/about.js
+++ src/about.js
@@ -31,8 +31,8 @@ function render(){
     title.textContent = "The Fictional Restaurant"
     aboutP1.textContent = 'The Fictional Restaurant serves best breakfast enriched with freshly baked pies. There is seating both inside the Manor House and on the patio beside the hotel gardens. A classic menu with refined seasonal pie dishes  elevating the best in local produce is served alongside choice wines from the valley.'
     aboutP2.textContent = 'We are family owned and have no plans to franchise or sell out.  We are committed to serving the best pie in Idaho and we believe that staying family owned is the only way to do that.  We open a few new restaurants each year to serve a few more customers.  If you’ve been with us from the beginning, or joined any time along the way – Thank You!  If you haven’t yet discovered our Fictional Restaurant, we invite you to come on in.   We promise it’s a habit you’ll come to love.'
-    aboutWorkTime.textContent = 'Work time';
-    aboutP3.textContent = 'Monday to Friday 7PM - 15PM';
+    aboutWorkTime.textContent = 'Open from';
+    aboutP3.textContent = 'Monday to Friday 7AM - 3PM';
     
     container1.appendChild(imgBorder1);
     container1.appendChild(aboutP1);
